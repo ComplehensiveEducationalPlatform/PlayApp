270f753192e946e4032c630fb7d040ac499eb943
KazuyaTomita <iamtommy1125@icloud.com>
2017-03-15 12:18:40 +0900

    modifying Schedule.java
    Scheduleクラスを修正。

--- app/Models/entity/Schedule.java
+++ app/Models/entity/Schedule.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import java.util.Date;
+import java.util.List;
+
 
 /**
  * Created by ve on 2017/03/10.
@@ -13,10 +15,29 @@ import java.util.Date;
 
 @Entity
 public class Schedule extends Model {
+
+    /**
+     * ScheduleのID
+     */
     @Id
     public long scheduleId;
+
+    /**
+     * このScheduleが設定される日時
+     */
     public Date date;
-    public boolean schedule;
+
+    /**
+     * スケジュールの詳細を示すboolean型の配列
+     * trueが対応可能、falseが対応不可を表す
+     * 例えば、scheduleArray[1]=trueであるならば、
+     * 0:30-1:00まで対応可能だということになる
+     */
+    public boolean[] scheduleArray = new boolean[48];
+
+    /**
+     * このScheduleクラスに紐づけられるUser
+     */
     @OneToOne
     public User user;
 
