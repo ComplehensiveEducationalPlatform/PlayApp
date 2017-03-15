4e5efb7d38a9c315afeb7da74f6c2301b47b8c37
KazuyaTomita <iamtommy1125@icloud.com>
2017-03-15 11:45:03 +0900

    modifying Question class
    Questionクラスを修正。

--- app/Models/entity/Question.java
+++ app/Models/entity/Question.java
@@ -14,25 +14,63 @@ import java.util.List;
 
 @Entity
 public class Question extends Model {
+
+    /**
+     * QuestionクラスのID
+     */
     @Id
     public long questionId;
+
+    /**
+     * Questionのタイトル
+     */
     @Constraints.Required
     public String title;
+
+    /**
+     * Questionの内容
+     */
     @Constraints.Required
     public String questionContent;
+
+    /**
+     * 質問の評価数
+     */
     public int questionReputationNumber;
+
+    /**
+     * クリップされた数
+     */
     public int clipNumber;
+
+    /**
+     * 投稿された日時
+     */
     @Column(name = "posted_time")
     public Date postedTime;
 
+    /**
+     * 質問に答えた時に与えられる額
+     */
     public int award;
+
+    /**
+     * 質問したUser
+     */
     @OneToOne
     public User user;
 
+    /**
+     * 質問にタグづけられたTag
+     */
     @OneToMany
-    public List<Tag> tagList;
+    public List<Tag> tagsList;
+
+    /**
+     * この質問に対する答え
+     */
     @OneToMany
-    public List<Answer> answerList;
+    public List<Answer> answersList;
 
     public Finder<Long, Question> finder = new Finder<Long, Question>(Question.class);
 }
